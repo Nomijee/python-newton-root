2ddb6803491792ccad7af6845ae59cf301c791b2
Muhammad Noman Junaid <g00351754@gmit.ie>
2020-02-20 05:13:05 +0000

    Added Aqure root function

---
+++ calcsqrt.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+
+# Muhammad Noman Junaid 
+# Calculate the sq. root of a number.
+
+def sqrt(x): 
+  """
+  Calculate the Sq. root of argument x.
+  """
+  
+  #Check that x is positive.
+  if x < 0:
+    print ("Error: negative value supplied")
+    return -1
+  else:
+    print("Here we go..")
+  
+  #Initial guess for the Sq. root.
+  z = x / 2.0
+
+  #Continuously improve the guess.
+  #Adapted from https://tour.golang.org/flowcontrol/8
+  while abs(x - (z*z)) > 0.000001:
+    z = z - (((z * z) - x) / (2 * z))
+  return z
+
+myval = 64.0
+print ("The Sq. Root of",myval, "is",sqrt(myval))
+
